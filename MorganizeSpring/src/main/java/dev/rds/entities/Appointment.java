df1fb6d0cf66a9121facb86e6bb578086530ee81
Sam Elliott <samelliott4@github,com>
2019-10-24 18:37:40 -0400

    Filled out methods and fixed naming error

--- MorganizeSpring/src/main/java/dev/rds/entities/Appointment.java
+++ MorganizeSpring/src/main/java/dev/rds/entities/Appointment.java
@@ -16,15 +16,50 @@ public class Appointment {
 	public static Type ADMIN = Type.ADMIN;
 	
 	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User user;
+	@JoinColumn(name = "a_id")
+	private Account account;
 	
 	@ManyToOne
-	@JoinColumn(name = "event_id")
+	@JoinColumn(name = "e_id")
 	private Event event;
 	
 	@Column(name = "type")
 	@Enumerated(EnumType.STRING)
 	private Type type;
-	
+
+	public Appointment() {
+		super();
+	}
+
+	public Appointment(Account account, Event event, Type type) {
+		super();
+		this.account = account;
+		this.event = event;
+		this.type = type;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public Event getEvent() {
+		return event;
+	}
+
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
 }
