76c475aa74f0a83da479775b0fe5238fe272ffee
bobzap66 <oskar_talgeron@yahoo.com>
2019-10-25 10:33:40 -0400

    Added services package as well as Organization Repository

---
+++ MorganizeSpring/src/main/java/dev/rds/repositories/OrganizationRepository.java
@@ -0,0 +1,19 @@
+package dev.rds.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import dev.rds.entities.Organization;
+import dev.rds.entities.Tag;
+
+@Component
+@Repository
+public interface OrganizationRepository extends CrudRepository <Organization, Integer> {
+	
+	List<Organization> findOrganizationByName(String name);
+	List<Organization> findOrganizationByTag(Tag tag);
+
+}
