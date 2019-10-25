684a6301881950f4fbe40a93cbac653dc38ab1dd
bobzap66 <oskar_talgeron@yahoo.com>
2019-10-25 11:24:24 -0400

    Added Organization Service interface and implementation, as well as initial tests. Added annotations into app.

--- MorganizeSpring/src/main/java/dev/rds/app/MorganizeSpringApplication.java
+++ MorganizeSpring/src/main/java/dev/rds/app/MorganizeSpringApplication.java
@@ -2,8 +2,14 @@ package dev.rds.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("dev.rds")
+@EntityScan("dev.rds.entities")
+@EnableJpaRepositories("dev.rds.repository")
 public class MorganizeSpringApplication {
 
 	public static void main(String[] args) {
