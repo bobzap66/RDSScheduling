91e9eb2e6518a07cb56a121895d1391b1cd8222f
GTulacro <grantulacro@gmail.com>
2019-10-25 15:43:45 -0400

    needs testing after AccountServices Created

--- MorganizeSpring/src/main/java/dev/rds/controller/LoginController.java
+++ MorganizeSpring/src/main/java/dev/rds/controller/LoginController.java
@@ -1,11 +1,17 @@
 package dev.rds.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import dev.rds.entities.Account;
 
 @Component
 @Controller
@@ -16,8 +22,21 @@ public class LoginController
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
-	public String login()
+	public String login(@RequestBody Account account)
 	{
+		HttpServletRequest request;	
+		HttpServletResponse response;
+		
+		account = as.login(account);
+		
+		if(account != null)
+		{
+			return "Login Successful";
+		}
+		else
+		{
+			return "Login Successful";
+		}
 		
 	}
 }
