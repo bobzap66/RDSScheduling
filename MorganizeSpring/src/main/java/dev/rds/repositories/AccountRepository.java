a4cee10ad533b11cb5ba134453dc804c8e548e2f
GTulacro <grantulacro@gmail.com>
2019-10-25 12:06:01 -0400

    added test case for AccountRepository

--- MorganizeSpring/src/main/java/dev/rds/repositories/AccountRepository.java
+++ MorganizeSpring/src/main/java/dev/rds/repositories/AccountRepository.java
@@ -1,9 +1,12 @@
 package dev.rds.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import dev.rds.entities.Account;
 
+@Component
+@Repository
 public interface AccountRepository extends CrudRepository<Account, Integer> {
 	
 	Account findByUsername(String username);
