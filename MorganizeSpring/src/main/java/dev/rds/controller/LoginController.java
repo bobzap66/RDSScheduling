b04ae4929ae99e6282a2281677bf8c1b86e70999
GTulacro <grantulacro@gmail.com>
2019-10-26 16:03:36 -0400

    changed the login controller. it communicates correctly with front end

--- MorganizeSpring/src/main/java/dev/rds/controller/LoginController.java
+++ MorganizeSpring/src/main/java/dev/rds/controller/LoginController.java
@@ -1,43 +1,30 @@
 package dev.rds.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import dev.rds.entities.Account;
-import dev.rds.services.AccountServices;
+import dev.rds.services.AccountService;
 
-@Component
-@Controller
+@RestController
+@CrossOrigin
 public class LoginController 
 {
 	@Autowired
-	AccountServices as;
+	AccountService as;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
-	public String login(@RequestBody Account account)
-	{
-		HttpServletRequest request;	
-		HttpServletResponse response;
-		
-		account = as.login(account);
-		
-		if(account != null)
-		{
-			return "Login Successful";
-		}
-		else
-		{
-			return "Login Successful";
-		}
-		
+	public Account login(@RequestBody Account temp)
+	{	
+		temp = as.login(temp);	
+		return temp;
 	}
+	
 }
