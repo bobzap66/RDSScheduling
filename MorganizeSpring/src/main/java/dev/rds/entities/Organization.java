66182a8b9836ea40705b31290bf724cdc21ca10c
Sam Elliott <samelliott4@github,com>
2019-11-03 18:18:15 -0500

    fixed update tags bug

--- MorganizeSpring/src/main/java/dev/rds/entities/Organization.java
+++ MorganizeSpring/src/main/java/dev/rds/entities/Organization.java
@@ -106,6 +106,30 @@ public class Organization
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Organization other = (Organization) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+
+	@Override
 	public String toString() {
 		return "Orginization [id=" + id + ", name=" + name + ", description=" + description + ", tags=" + tags
 				+ ", events=" + events + ", memberships=" + memberships + "]";
